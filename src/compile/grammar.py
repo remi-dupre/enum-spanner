23f5fc2cc6e7dbec4a0c096f9b246daad476503f
Rémi Dupré <remim.dupre@gmail.com>
2019-03-21 13:46:57 +0100

    compile: describe basic grammar

---
+++ src/compile/grammar.py
@@ -0,0 +1,29 @@
+import logging
+from lark import Lark
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+GRAMMAR = '''
+    start:                                                  -> empty
+         | expression
+
+    ?charclass: charclass charclass
+              | LETTER "-" LETTER                           -> range
+
+    ?atom: LETTER                                           -> letter
+        | "[" charclass "]"                                 -> charclass
+        | "."                                               -> wildcard
+
+    ?expression: atom
+               | expression expression                      -> concatenate
+               | expression "|" expression                  -> union
+               | expression "*"                             -> star
+               | expression "?"                             -> optional
+               | "(" expression ")"                         -> block
+
+    %import common.LETTER
+'''
+
+# Build the AST, given an input regexp
+build_ast = Lark(GRAMMAR, parser='lalr', debug=True).parse
