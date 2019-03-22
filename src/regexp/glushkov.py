bf4bc04001c51b8b1aecbf8fc77ac7c4febe10d7
Rémi Dupré <remim.dupre@gmail.com>
2019-03-22 14:22:05 +0100

    glushkov: save factors in lists instead of sets

--- src/regexp/glushkov.py
+++ src/regexp/glushkov.py
@@ -26,46 +26,48 @@ class ASTtoNFA(Transformer):
         transitions = (
             [(source, self.atoms[target], target) for source, target in D]
             + [(0, self.atoms[target], target) for target in P])
-        finals = F if not G else F | {0}
+        finals = F if not G else F + [0]
 
         return VA(self.nb_atoms + 1, transitions, finals)
 
+    #pylint: disable=no-self-use,too-many-locals
     def concatenation(self, sub):
         (lP, lD, lF, lG), (rP, rD, rF, rG) = sub
 
-        P = lP if not lG else lP | rP
-        F = rF if not rG else lF | rF
+        P = lP if not lG else lP + rP
+        F = rF if not rG else lF + rF
         G = lG and rG
-
-        D = lD | rD
+        D = lD + rD
 
         for prefix in lF:
             for suffix in rP:
-                D.add((prefix, suffix))
+                D.append((prefix, suffix))
 
         return P, D, F, G
 
     def charclass(self, sub):
         atom = self.register_atom(atoms.CharClass(sub))
-        return {atom}, set(), {atom}, False
+        return [atom], [], [atom], False
 
     def charclass_complement(self, sub):
         atom = self.register_atom(atoms.CharClassComplement(sub))
-        return {atom}, set(), {atom}, False
+        return [atom], [], [atom], False
 
+    #pylint: disable=no-self-use
     def class_escaped_char(self, sub):
         return str(sub[0]), str(sub[0])
 
     def class_normal_char(self, sub):
         return str(sub[0]), str(sub[0])
 
+    #pylint: disable=unused-argument
     def empty(self, sub):
-        return set(), set(), set(), True
+        return [], [], [], True
 
     def escaped_char(self, sub):
         char = str(sub[0])
         atom = self.register_atom(atoms.Char(char))
-        return {atom}, set(), {atom}, False
+        return [atom], [], [atom], False
 
     def named_group(self, sub):
         name, (P, D, F, G) = sub
@@ -74,22 +76,21 @@ class ASTtoNFA(Transformer):
         open_id = self.register_atom(variable.marker_open())
         close_id = self.register_atom(variable.marker_close())
 
-        nP = {open_id}
-        nD = (D
-              | {(open_id, target) for target in P}
-              | {(source, close_id) for source in F})
-        nF = {close_id}
+        nP = [open_id]
+        nD = (D + [(open_id, target) for target in P]
+              + [(source, close_id) for source in F])
+        nF = [close_id]
         nG = False
 
         if G:
-            nD.add((open_id, close_id))
+            nD.append((open_id, close_id))
 
         return nP, nD, nF, nG
 
     def normal_char(self, sub):
         char = str(sub[0])
         atom = self.register_atom(atoms.Char(char))
-        return {atom}, set(), {atom}, False
+        return [atom], [], [atom], False
 
     def optional(self, sub):
         P, D, F, _ = sub[0]
@@ -100,7 +101,7 @@ class ASTtoNFA(Transformer):
 
         for suffix in P:
             for prefix in F:
-                D.add((prefix, suffix))
+                D.append((prefix, suffix))
 
         return P, D, F, G
 
@@ -112,20 +113,20 @@ class ASTtoNFA(Transformer):
 
         for suffix in P:
             for prefix in F:
-                D.add((prefix, suffix))
+                D.append((prefix, suffix))
 
         return P, D, F, True
 
     def union(self, sub):
         (lP, lD, lF, lG), (rP, rD, rF, rG) = sub
 
-        P = lP | rP
-        F = lF | rF
-        D = lD | rD
+        P = lP + rP
+        F = lF + rF
+        D = lD + rD
         G = lG or rG
 
         return P, D, F, G
 
     def wildcard(self, sub):
         atom = self.register_atom(atoms.Wildcard())
-        return {atom}, set(), {atom}, False
+        return [atom], [], [atom], False
