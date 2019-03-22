da6d0205af0a5f8f684c68d5d7cc4d4f2e95374c
Rémi Dupré <remim.dupre@gmail.com>
2019-03-22 14:09:53 +0100

    match: add pretty_print

--- src/match.py
+++ src/match.py
@@ -1,3 +1,5 @@
+from collections import deque
+from termcolor import cprint
 
 
 class Match:
@@ -11,6 +13,25 @@ class Match:
     def string(self):
         begin, end = self.span
         return self.document[begin:end]
+
+    def pretty_print(self):
+        symbols = {i : deque() for i in range(len(self.document) + 1)}
+
+        for group, (l, r) in self.group_spans.items():
+            symbols[l].append(f'[⊢{group}]')
+            symbols[r].appendleft(f'[{group}⊣]')
+
+
+        for i in range(len(self.document) + 1):
+            cprint(''.join(symbols[i]), 'red', attrs=['bold', 'dark'], end='')
+
+            if i < len(self.document):
+                if self.span[0] <= i <= self.span[1]:
+                    cprint(self.document[i], 'red', attrs=['bold'], end='')
+                else:
+                    cprint(self.document[i], end='')
+
+        cprint('')
 
     def __repr__(self):
         return f'Match(span={self.span}, match=\'{self.string}\')'
