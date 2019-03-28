4bb7fff61be3a46225e8a8653868c0ac4eb87747
Rémi Dupré <remim.dupre@gmail.com>
2019-03-28 15:39:56 +0100

    main.py: add --display-offset, --only-matching and --no-print (fix #2)

--- src/main.py
+++ src/main.py
@@ -7,14 +7,19 @@ import regexp
 from enum_mappings import enum_matches
 
 
+sys.setrecursionlimit(10**4)
+
 # ----- Parse Command Line Arguments -----
 
 parser = argparse.ArgumentParser(
     description='Enumerate all matches of a regular expression on a text.')
 
 
-parser.set_defaults(debug=True)
 parser.set_defaults(count=False)
+parser.set_defaults(debug=True)
+parser.set_defaults(display_offset=False)
+parser.set_defaults(only_matching=False)
+parser.set_defaults(print=True)
 parser.set_defaults(show_automata=False)
 
 
@@ -26,12 +31,26 @@ parser.add_argument(
     help='the file to be read, if none is specified, STDIN is used')
 
 parser.add_argument(
+    '-b', '--byte-offset', dest='display_offset', action='store_true',
+    help='Print the 0-based offset of each matching part and groups.')
+
+parser.add_argument(
     '-c', '--count', dest='count', action='store_true',
     help='display the number of matches instead')
 
 parser.add_argument(
     '-d', '--debug', dest='debug', action='store_true',
     help='display debug information')
+
+parser.add_argument(
+    '-o', '--only-matching', dest='only_matching', action='store_true',
+    help='Print only the matched (non-empty) parts of a matching line, with '
+         'each such part on a separate output line.')
+
+parser.add_argument(
+    '-p', '--no-print', dest='print', action='store_false',
+    help='Prevent the display of a substring for each match')
+
 parser.add_argument(
     '--no-debug', dest='debug', action='store_false',
     help='don\'t display debug information')
@@ -63,8 +82,19 @@ if args.count:
     print(sum(1 for _ in enum_matches(pattern, document)))
 else:
     for count, match in enumerate(enum_matches(pattern, document)):
-        print(f'{count}: ', end='')
-        match.pretty_print()
+        if args.display_offset or not args.print:
+            print(f'{match.span[0]},{match.span[1]}', end='')
+
+            for name, span in match.group_spans.items():
+                print(f' {name}={span[0]},{span[1]}', end='')
+
+            if args.print:
+                print(': ', end='')
+
+        if args.print:
+            match.pretty_print(args.only_matching)
+        else:
+            print()
 
 
 # ----- Print Debug Infos -----
