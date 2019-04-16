4aaed1c7a0aa3a3ff3634bd5d674dff1317bdf22
Rémi Dupré <remim.dupre@gmail.com>
2019-04-16 14:34:03 +0200

    reorganize explore_dag.py and precompute_dag.py in IndexedDag and Jump

--- src/enum_mappings/__init__.py
+++ src/enum_mappings/__init__.py
@@ -1,35 +1,33 @@
-from dag import DAG, EmptyLangage
-from enum_mappings.explore_dag import enum_dag_mappings
-from enum_mappings.precompute_dag import IndexedDag
+from enum_mappings.indexed_dag import IndexedDag
+from enum_mappings.jump import EmptyLevel
 from enum_mappings.naive import naive_enum_mappings
 from mapping import match_of_mapping
 from va import VA
 
 
-def compile_matches(va: VA, text: str) -> DAG:
+def compile_matches(va: VA, text: str) -> IndexedDag:
     '''
-    Compile the list of matches of a va over a text into a DAG.
+    Compile the list of matches of a Variable Automata over a text into a DAG.
     '''
-    #  dag = product_dag(va, text)
-    #  dag.remove_useless_nodes(check_accessible=False)
-    #  return dag
-    return NotImplemented
+    return IndexedDag(va, text)
+
 
 def enum_mappings(va: VA, text: str):
+    '''
+    Iterate over the mappings of the given Variable Automaton over a text.
+    '''
     try:
-        dag = IndexedDag(va, text)
-    except EmptyLangage:
+        dag = compile_matches(va, text)
+    except EmptyLevel:
         return iter([])
 
-    #  for source, target in dag.reach:
-    #      print(f'============ {source} -> {target}')
-    #      print(f'{source}: {dag.levelset.vertices[source]}')
-    #      print(f'{target}: {dag.levelset.vertices[target]}')
-    #      print(dag.reach[source, target])
+    return iter(dag)
 
-    return enum_dag_mappings(dag)
 
 def enum_matches(va: VA, text: str):
+    '''
+    Iterate over the matches of the given Variable Automaton over a text.
+    '''
     for mapping in enum_mappings(va, text):
         match = match_of_mapping(text, va.variables, mapping)
 
